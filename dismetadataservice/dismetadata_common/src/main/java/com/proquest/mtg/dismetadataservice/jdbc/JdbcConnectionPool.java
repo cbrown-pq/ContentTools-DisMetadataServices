2109046164c798bb69ad3caee558a06e0bf149f8
mnarayanpq <Mahitha.Narayan@proquest.com>
2013-12-11 09:08:47 -0500

    Added Connection pooling.

---
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/jdbc/JdbcConnectionPool.java
@@ -0,0 +1,50 @@
+package com.proquest.mtg.dismetadataservice.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import oracle.ucp.UniversalConnectionPoolAdapter;
+import oracle.ucp.admin.UniversalConnectionPoolManager;
+import oracle.ucp.admin.UniversalConnectionPoolManagerImpl;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+
+public class JdbcConnectionPool implements IJdbcConnectionPool {
+
+	private static volatile int poolCount = 0;
+	private final PoolDataSource poolDataSource;
+
+	public JdbcConnectionPool(JdbcConfig config) throws Exception {
+
+		poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+		poolDataSource.setURL(config.getDbUrl());
+		poolDataSource.setUser(config.getUserName());
+		poolDataSource.setPassword(config.getPassword());
+		poolDataSource.setConnectionFactoryClassName(config.getDbClassType());
+		poolDataSource.setMaxConnectionReuseTime(5 * 60);
+		poolDataSource.setMaxConnectionReuseCount(100);
+		poolDataSource.setAbandonedConnectionTimeout(1 * 60);
+		poolDataSource.setInactiveConnectionTimeout(1 * 60);
+		
+		poolDataSource.setConnectionPoolName("JdbcConnectionPool - " + (++poolCount));
+		
+		UniversalConnectionPoolManager poolManager = 
+				UniversalConnectionPoolManagerImpl.getUniversalConnectionPoolManager();
+		
+		poolManager.createConnectionPool((UniversalConnectionPoolAdapter)poolDataSource);
+	}
+
+	public Connection getConnection() throws SQLException {
+		return poolDataSource.getConnection();
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		UniversalConnectionPoolManager poolManager = 
+				UniversalConnectionPoolManagerImpl.getUniversalConnectionPoolManager();
+		
+		poolManager.destroyConnectionPool(poolDataSource.getConnectionPoolName());
+	}
+
+}
---
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/jdbc/JdbcHelper.java
@@ -0,0 +1,51 @@
+package com.proquest.mtg.dismetadataservice.jdbc;
+
+public class JdbcHelper {
+	
+	public static final String kConnectionFactoryClass_OracleThin = "oracle.jdbc.pool.OracleDataSource";
+	
+	public static JdbcConfig makeConfigForExodusDevelopment() {
+		String url = "jdbc:oracle:thin:@shrdb304.aa1.pqe:1521:exdvl";
+		String user = "mnarayan";
+		String password = "mnarayan";
+		String dbClassType = kConnectionFactoryClass_OracleThin;
+		int maxPoolSize = 5;
+		return new JdbcConfig(url, user, password, dbClassType, maxPoolSize);
+	}
+	
+	public static JdbcConfig makeConfigForExodusPreProd() {
+		String url = "jdbc:oracle:thin:@shrdb302.aa1.pqe:1521:expep";
+		String user = "dos_pep";
+		String password = "dos_pep";
+		String dbClassType = kConnectionFactoryClass_OracleThin;
+		int maxPoolSize = 5;
+		return new JdbcConfig(url, user, password, dbClassType, maxPoolSize);
+	}
+	
+	public static JdbcConfig makeConfigForExodusProd() {
+		String url = "jdbc:oracle:thin:@shrdb102.dc4.pqe:1521:exprd";
+		String user = "dos_prod";
+		String password = "dos_prod";
+		String dbClassType = kConnectionFactoryClass_OracleThin;
+		int maxPoolSize = 5;
+		return new JdbcConfig(url, user, password, dbClassType, maxPoolSize);
+	}	
+
+	
+	public static JdbcConnectionPool makePoolForExodusDevelopment() throws Exception {		
+		return new JdbcConnectionPool(makeConfigForExodusDevelopment());
+	}
+	
+	public static JdbcConnectionPool makePoolForExodusPreProd() throws Exception {		
+		return new JdbcConnectionPool(makeConfigForExodusPreProd());
+	}
+	
+	public static JdbcConnectionPool makePoolForExodusProd() throws Exception {		
+		return new JdbcConnectionPool(makeConfigForExodusProd());
+	}
+	
+	public static JdbcConnectionPool makePoolForExodusUnitTest() throws Exception {		
+		return new JdbcConnectionPool(makeConfigForExodusProd());
+	}	
+
+}
