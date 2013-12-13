5d4c3ae7b5a2af9a81556dca6b280e20bdb4071d
Jyotirmoy Chakraborty <jyotirmoy.chakraborty@proquest.com>
2013-12-13 11:50:20 -0500

    Adding DisPubMetaData class

---
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/exodus/DisPubMetaData.java
@@ -0,0 +1,14 @@
+package com.proquest.mtg.dismetadataservice.exodus;
+
+public class DisPubMetaData {
+	private String pubNumber;
+	private String itemId;
+	
+	public String getPubNumber() {
+		return pubNumber;
+	}
+	
+	public String getItemId() {
+		return itemId;
+	}
+}
