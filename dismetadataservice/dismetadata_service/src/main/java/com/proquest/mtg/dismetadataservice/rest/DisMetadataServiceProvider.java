0f2ec6cb7ae9ae509e2166e1b7c75b65ed0c8209
Mahitha Narayan <Mahitha.Narayan@proquest.com>
2013-12-18 11:50:09 -0500

    Updated DisMetadataProvider to include the metadata extracted by pubmetadataprovider class and publish the result in response message.

--- dismetadataservice/dismetadata_service/src/main/java/com/proquest/mtg/dismetadataservice/rest/DisMetadataServiceProvider.java
+++ dismetadataservice/dismetadata_service/src/main/java/com/proquest/mtg/dismetadataservice/rest/DisMetadataServiceProvider.java
@@ -7,22 +7,48 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData;
+import com.proquest.mtg.dismetadataservice.exodus.PubMetaDataProvider;
 
 @Path("/dispubmetadata/")
 public class DisMetadataServiceProvider {
+	private final PubMetaDataProvider pubMetadataProvider;
+
+	@Inject
+	public DisMetadataServiceProvider(PubMetaDataProvider pubMetadataProvider) {
+		this.pubMetadataProvider = pubMetadataProvider;
+	}
+
+	public PubMetaDataProvider getPubMetadataProvider() {
+		return pubMetadataProvider;
+	}
 
 	@GET
 	@Path("/{pubNumber}/{formatType}")
-	@Produces(MediaType.APPLICATION_XML)
-	public Response getDisMetaData(
-			@PathParam("pubNumber") String pubNumber,
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getDisMetaData(@PathParam("pubNumber") String pubNumber,
 			@PathParam("formatType") String formatType) {
+		String result = null;
+		DisPubMetaData disPubMetadata;
+		try {
+			disPubMetadata = pubMetadataProvider
+					.getPubMetaDataFor(pubNumber);
+			result = "Pub Number: " + disPubMetadata.getPubNumber() + "\n" 	+ 
+					 "Item Id: " + disPubMetadata.getItemId() + "\n" + 
+					 "ISBN: " + disPubMetadata.getISBN() + "\n" + 
+					 "Pub Page Number: "  + disPubMetadata.getPubPageNum() + "\n" + 
+					 "Page Count: "	+ disPubMetadata.getPageCount() + "\n" +
+					 "Format Type: " + formatType;
+		} catch (NullPointerException e) {
+			result = "No Data Found";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		DisPubMetaData disPubMetadata = new DisPubMetaData();
-		disPubMetadata.setPubNumber(pubNumber);
-		return Response.status(200).entity(disPubMetadata).build();
+		return Response.status(200).entity(result).build();
 
 	}
-	
+
 }
