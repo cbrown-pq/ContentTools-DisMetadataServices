789f83d204039e817d7c30b3d283d2b4c20201dc
Mahitha Narayan <Mahitha.Narayan@proquest.com>
2013-12-30 13:55:08 -0500

    Added more marc records to MarcRecord Factory.

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/marc/MarcRecordFactory.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/marc/MarcRecordFactory.java
@@ -4,47 +4,47 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData;
+import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData.Advisor;
 import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData.DissLanguage;
 import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData.Subject;
+import com.proquest.mtg.dismetadataservice.metadata.Author.Degree;
 import com.proquest.mtg.dismetadataservice.metadata.TextNormalizer;
 
 public class MarcRecordFactory {
-	
+
 	public static final String kRecordIdPrefix = "AAI";
-	
+
 	private final TextNormalizer abstractNormalizer = new TextNormalizer();
-	
+
 	private DisPubMetaData curMetaData = null;
 	private MarcRecord curRecord = null;
-	
+
 	public MarcRecord makeFrom(DisPubMetaData metaData) {
 		if (null == metaData) {
 			throw new IllegalArgumentException("metaData is null");
 		}
 		curMetaData = metaData;
 		curRecord = new MarcRecord();
-		
+
 		handleRecordId();
 		handleAbstract();
 		handleLocationOfCopy();
 		handleSubjects();
+		handleAdvisor();
+		handleDegrees();
 		handleDisserationLanguage();
 		handleUrl();
-		
+
 		return curRecord;
 	}
-
-
-
 
 	private void handleRecordId() {
 		String pubId = curMetaData.getPubNumber();
 		if (null != pubId && !pubId.isEmpty()) {
-			addField(MarcTags.kRecId, 
-					kRecordIdPrefix + pubId.trim());
+			addField(MarcTags.kRecId, kRecordIdPrefix + pubId.trim());
 		}
 	}
-	
+
 	private void handleAbstract() {
 		String abstractText = curMetaData.getAbstract();
 		if (null != abstractText && !abstractText.isEmpty()) {
@@ -59,7 +59,8 @@ public class MarcRecordFactory {
 
 	private List<String> makeAbstractParagraphsFrom(String abstractText) {
 		List<String> result = Lists.newArrayList();
-		for (String curParagraph : abstractText.split("\\^|\n|<[pP]>|</[pP]>|<[pP][aA][rR]>|</[pP][aA][rR]>")) {
+		for (String curParagraph : abstractText
+				.split("\\^|\n|<[pP]>|</[pP]>|<[pP][aA][rR]>|</[pP][aA][rR]>")) {
 			if (null != curParagraph) {
 				curParagraph = curParagraph.trim();
 				if (!curParagraph.isEmpty()) {
@@ -69,7 +70,7 @@ public class MarcRecordFactory {
 		}
 		return result;
 	}
-	
+
 	private String endsWithPunctuationMark(String x) {
 		return x.matches("^.+[\\.,\\?;:!]$") ? x : x + ".";
 	}
@@ -82,9 +83,9 @@ public class MarcRecordFactory {
 					makeFieldDataFrom('2', ' ', 'a', locationOfCopy));
 		}
 	}
-	
+
 	private void handleSubjects() {
-		List<Subject> subjects = curMetaData.getSubjects(); 
+		List<Subject> subjects = curMetaData.getSubjects();
 		if (!subjects.isEmpty()) {
 			for (Subject curSubject : subjects) {
 				addSubjectTerm(curSubject.getSubjectDesc());
@@ -100,12 +101,39 @@ public class MarcRecordFactory {
 					makeFieldDataFrom(' ', '4', 'a', term));
 		}
 	}
-	
+
 	private void addSubjectCode(String code) {
 		if (null != code && !code.isEmpty()) {
 			addField(MarcTags.kSubjectCode,
 					makeFieldDataFrom(' ', ' ', 'a', code));
 		}
+	}
+	
+	private void handleAdvisor() {
+		List<Advisor> dissAdvisors = curMetaData.getAdvisors();
+		if (!dissAdvisors.isEmpty()) {
+			for (Advisor curAdvisor : dissAdvisors) {	
+				String adviserString = makeFieldDataFrom(' ',',','e',"advisor");
+				addField(
+						MarcTags.kAdvisorname,makeFieldDataFrom('1', '0', 'a' ,curAdvisor.getAdvisorFullName(), adviserString));
+			}
+		}
+
+		
+	}
+	private void handleDegrees() {
+		List<Degree> degrees = curMetaData.getAuthors().get(0).getDegrees();
+		if (!degrees.isEmpty()) {
+			for (Degree curDegree : degrees) {
+				if (curDegree.getSequenceNumber() == 1) {
+					addField(
+							MarcTags.kDegreeName,makeFieldDataFrom(' ', ' ', 'a',curDegree.getDegreeCode()));
+					addField(
+							MarcTags.kDegreeDate,makeFieldDataFrom(' ', ' ', 'a',curDegree.getDegreeYear()));
+				}
+			}
+		}
+
 	}
 
 	private void handleDisserationLanguage() {
@@ -116,44 +144,38 @@ public class MarcRecordFactory {
 			}
 		}
 	}
-	
+
 	private void handleUrl() {
 		String url = curMetaData.getExternalURL();
 		String pubId = curMetaData.getPubNumber();
-		if(null != url && !url.isEmpty()) {
+		if (null != url && !url.isEmpty()) {
 			addField(MarcTags.kUrl,
 					makeFieldDataFrom(' ', ' ', 'u', url, pubId.trim()));
 		}
-		
+
 	}
-	
+
 	private String endWithPeriod(String x) {
 		return x.endsWith(".") ? x : x + ".";
 	}
-	
+
 	private void addField(String tag, String data) {
 		curRecord.addField(new MarcField(tag, data));
 	}
-	
-	private String makeFieldDataFrom(
-			char dataFieldIndicator1,
-			char dataFieldIndicator2,
-			char subFieldIndicator,
-			String fieldData) {
+
+	private String makeFieldDataFrom(char dataFieldIndicator1,
+			char dataFieldIndicator2, char subFieldIndicator, String fieldData) {
 		StringBuilder builder = new StringBuilder();
 		builder.append(dataFieldIndicator1);
 		builder.append(dataFieldIndicator2);
 		builder.append(MarcCharSet.kSubFieldIndicator);
 		builder.append(subFieldIndicator);
 		builder.append(fieldData);
-		System.out.println("dsfdsfdsfsdf:" + builder.toString());
 		return builder.toString();
 	}
 
-	private String makeFieldDataFrom(
-			char dataFieldIndicator1,
-			char dataFieldIndicator2,
-			char subFieldIndicator,
+	private String makeFieldDataFrom(char dataFieldIndicator1,
+			char dataFieldIndicator2, char subFieldIndicator,
 			String fieldData1, String fieldData2) {
 		StringBuilder builder = new StringBuilder();
 		builder.append(dataFieldIndicator1);
@@ -162,7 +184,7 @@ public class MarcRecordFactory {
 		builder.append(subFieldIndicator);
 		builder.append(fieldData1);
 		builder.append(fieldData2);
-		System.out.println("dsfdsfdsfsdf:" + builder.toString());
 		return builder.toString();
-	}
+	}	
+
 }
