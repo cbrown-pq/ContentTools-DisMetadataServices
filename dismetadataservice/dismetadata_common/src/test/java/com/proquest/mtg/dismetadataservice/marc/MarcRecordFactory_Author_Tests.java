082c7be98b263f9ee8e5170c509593d590a66c8a
Nil Tripathy <nilamadhaba.tripathy@proquest.com>
2014-01-13 14:49:23 -0500

    Added Unit Test cases for Author marc field

---
+++ dismetadataservice/dismetadata_common/src/test/java/com/proquest/mtg/dismetadataservice/marc/MarcRecordFactory_Author_Tests.java
@@ -0,0 +1,85 @@
+package com.proquest.mtg.dismetadataservice.marc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData;
+import com.proquest.mtg.dismetadataservice.exodus.DisPubMetaData.DissLanguage;
+import com.proquest.mtg.dismetadataservice.jdbc.IJdbcConnectionPool;
+import com.proquest.mtg.dismetadataservice.jdbc.JdbcHelper;
+import com.proquest.mtg.dismetadataservice.metadata.Author;
+import com.proquest.mtg.dismetadataservice.metadata.Author.Degree;
+import com.proquest.mtg.dismetadataservice.metadata.DisGenMappingProvider;
+
+public class MarcRecordFactory_Author_Tests {
+	
+static int kDataIndependentFieldCount = 3;
+	
+	DisGenMappingProvider disGenMappingProvider;
+	Author author;
+	List<Author> authors;
+	Degree degree;
+	List<Degree> degrees;
+	
+	DissLanguage language;
+	List<DissLanguage> languages;
+	
+	MarcRecordFactory factory;
+	
+	@Before
+	public void setUp() throws Exception {
+		IJdbcConnectionPool connectionPool = JdbcHelper.makePoolForExodusUnitTest();
+		DisGenMappingProvider disGenMappingProvider = new DisGenMappingProvider(connectionPool);
+		factory = new MarcRecordFactory(disGenMappingProvider);
+	}
+	
+	
+	@Test
+	public void withNullAuthorName_ShouldNotGenerateTag() {
+		author = new Author();
+		authors = Lists.newArrayList(author);
+		String tag = MarcTags.kAuthor;
+		DisPubMetaData metaData = new DisPubMetaData();
+		metaData.setAuthors(authors);
+		MarcRecord marc = factory.makeFrom(metaData);
+		List<MarcField> fieldsMatchingTag = marc.getFieldsMatchingTag(tag); 
+		assertThat(fieldsMatchingTag.size(), is(0));
+	}
+	
+	@Test
+	public void authorWithoutSGMLChar() {
+		author = new Author();
+		author.setAuthorFullName("John C Mark");
+		authors = Lists.newArrayList(author);
+		String tag = MarcTags.kAuthor;
+		DisPubMetaData metaData = new DisPubMetaData();
+		metaData.setAuthors(authors);
+		MarcRecord marc = factory.makeFrom(metaData);
+		String expectedData = "1 " + MarcCharSet.kSubFieldIndicator + "a" + "John C Mark" + ".";
+		List<MarcField> fieldsMatchingTag = marc.getFieldsMatchingTag(tag); 
+		assertThat(fieldsMatchingTag.size(), is(1));
+		assertThat(fieldsMatchingTag.get(0).getData(), is(expectedData));
+	}
+	
+	@Test
+	public void authorWithSGMLChar() {
+		author = new Author();
+		author.setAuthorFullName("&prime;John C &AElig; Mark@?");
+		authors = Lists.newArrayList(author);
+		String tag = MarcTags.kAuthor;
+		DisPubMetaData metaData = new DisPubMetaData();
+		metaData.setAuthors(authors);
+		MarcRecord marc = factory.makeFrom(metaData);
+		String expectedData = "1 " + MarcCharSet.kSubFieldIndicator + "a" + "''John C AE Mark" + ".";
+		List<MarcField> fieldsMatchingTag = marc.getFieldsMatchingTag(tag); 
+		assertThat(fieldsMatchingTag.size(), is(1));
+		assertThat(fieldsMatchingTag.get(0).getData(), is(expectedData));
+	}
+
+}
