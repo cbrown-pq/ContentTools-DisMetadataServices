474b03714864464c893b81933d502897ffd55679
Jyotirmoy Chakraborty <jyotirmoy.chakraborty@proquest.com>
2014-12-01 17:10:58 -0500

    Committing the Java based PUT client

--- dismetadataservice/dismetadata_utils/src/main/java/com/proquest/mtg/dismetadataservice/PutRequestClient.java
+++ dismetadataservice/dismetadata_utils/src/main/java/com/proquest/mtg/dismetadataservice/PutRequestClient.java
@@ -14,7 +14,8 @@ public class PutRequestClient {
 		String password = "pw4school";
 		Client c = Client.create();
 		c.addFilter(new HTTPBasicAuthFilter(clientName, password));
-		WebResource resource = c.resource("http://localhost:8080/dismetadata_service/disout/schoolmetadata/ackSchoolMetadataLoadFor/0056");
+		//WebResource resource = c.resource("http://localhost:8080/dismetadata_service/disout/schoolmetadata/ackSchoolMetadataLoadFor/0056");
+		WebResource resource = c.resource("http://umidos210.aa1.pqe:8001/dismetadata_service/disout/locreports/updatefilmpulldate?publist=1374510,1374511,1374518,1374520,1374525,1374537,1374539,1374546,1374552,1374555,1374562,1374574,1374576,1374579");
         ClientResponse response = resource.put(ClientResponse.class, null);
 	    
 	    int statusCode = response.getStatus();
