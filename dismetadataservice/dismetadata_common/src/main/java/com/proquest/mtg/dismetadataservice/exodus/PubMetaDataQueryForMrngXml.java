83bd86ee1bd72bd066571c5e596a3d02dc1785ff
chanchalbag <Cbag@J6Z7HV1.proque.st>
2015-12-11 12:36:35 -0500

    Fixed
    Fixed

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/exodus/PubMetaDataQueryForMrngXml.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/exodus/PubMetaDataQueryForMrngXml.java
@@ -816,9 +816,7 @@ public class PubMetaDataQueryForMrngXml {
 	private String processTextForPlatform(String x) {
 		String result = x;
 		if (null != x) {
-			result = result.replaceAll("&(amp;)+", "&amp;");
-			result = HtmlEscape.unescapeHtml(result);
-			// Call 2nd time to Handle character like &amp;Aring;
+			result = result.replaceAll("&(amp;)*", "&");
 			result = HtmlEscape.unescapeHtml(result);
 			result = textNormalizer.removeAdeptTags(result);
 			result = textNormalizer.addCdataTags(result);
