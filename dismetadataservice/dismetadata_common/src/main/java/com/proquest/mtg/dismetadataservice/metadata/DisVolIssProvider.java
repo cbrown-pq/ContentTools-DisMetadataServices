fe65071eaea468360a10e4ace6287269aff382f3
cbrown-pq <charles.brown@proquest.com>
2019-06-24 13:16:56 -0400

    Create DisVolIssProvider.java
    Temporary class to calculate vol/iss for dissertations based on create date from ecms/mr3

---
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/metadata/DisVolIssProvider.java
@@ -0,0 +1,80 @@
+package com.proquest.mtg.dismetadataservice.metadata;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+// This class is to temporarily generate the Vol/Iss field for Metadata output.
+// It should be replaced with data from ECMS at some point.
+public class DisVolIssProvider {
+	
+	public static String DisVolIssProvider(Integer pubYear,String pubMonth) {
+		// Today's date needs to be the publication date, which will be sent in
+	LocalDate initialDate = LocalDate.of(1938,Month.JANUARY,1);                          //Today's date
+	// Period required the month and day, but it will not be used as part of the calculation, except as required by Period.
+	LocalDate creationDate = LocalDate.of(pubYear, Month.valueOf(pubMonth), 1);  //Birth date
+	
+	Period p = Period.between(initialDate, creationDate);
+	 
+	//Now access the values as below
+	System.out.println("Days :" +p.getDays());
+	System.out.println("Months :" +p.getMonths());
+	System.out.println("Years :" +p.getYears());
+	int VolIss = p.getYears();
+	String dissVolIss = String.valueOf(VolIss);
+	System.out.println("STRING YEARS :" +dissVolIss);
+	// Since this is temporary, I did not try to make it efficient or pretty.
+	if (pubMonth.equals("JULY")) {
+		dissVolIss = (dissVolIss + '-' + "1");
+	}
+	if (pubMonth.equals("AUGUST")) {
+		dissVolIss = (dissVolIss + '-' + "2");
+	}
+	if (pubMonth.equals("SEPTEMBER")) {
+		dissVolIss = (dissVolIss + '-' + "3");
+	}
+	if (pubMonth.equals("OCTOBER")) {
+		dissVolIss = (dissVolIss + '-' + "4");
+	}
+	if (pubMonth.equals("NOVEMBER")) {
+		dissVolIss = (dissVolIss + '-' + "5");
+	}
+	if (pubMonth.equals("DECEMBER")) {
+		dissVolIss = (dissVolIss + '-' + "6");
+	}
+	if (pubMonth.equals("JANUARY")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "7");
+	}
+	if (pubMonth.equals("FEBRUARY")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "8");
+	}
+	if (pubMonth.equals("MARCH")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "9");
+	}
+	if (pubMonth.equals("APRIL")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "10");
+	}
+	if (pubMonth.equals("MAY")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "11");
+	}
+	if (pubMonth.equals("JUNE")) {
+		VolIss = VolIss -1;
+		dissVolIss = String.valueOf(VolIss);
+		dissVolIss = (dissVolIss + '-' + "12");
+	}
+	// If January - June, Subtract 1 from year
+	// Issue is 1 - 12 starting with July as 1.
+	return dissVolIss;
+	}
+
+}
