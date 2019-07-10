5045717cb16ef3e319a465fa656344682450c250
cbrown-pq <charles.brown@proquest.com>
2019-07-10 09:44:44 -0400

    Update MockServiceTestHelper.java
    removed other unused web services

--- dismetadataservice/dismetadata_service/src/main/java/com/proquest/mtg/dismetadataservice/rest/helper/MockServiceTestHelper.java
+++ dismetadataservice/dismetadata_service/src/main/java/com/proquest/mtg/dismetadataservice/rest/helper/MockServiceTestHelper.java
@@ -6,26 +6,26 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.proquest.mtg.dismetadataservice.format.MetaDataFormatFactory;
-import com.proquest.mtg.dismetadataservice.rest.ExternalUrlXmlServiceProvider;
+//import com.proquest.mtg.dismetadataservice.rest.ExternalUrlXmlServiceProvider;
 import com.proquest.mtg.dismetadataservice.rest.FOPUpdateAvailableFormatsServiceProvider;
 import com.proquest.mtg.dismetadataservice.rest.LocMetaDataServiceProvider;
 import com.proquest.mtg.dismetadataservice.rest.LocReportServiceProvider;
 //import com.proquest.mtg.dismetadataservice.rest.MStarXmlMetadataServiceProvider;
 import com.proquest.mtg.dismetadataservice.rest.PdfDownloadServiceProvider;
-import com.proquest.mtg.dismetadataservice.rest.SchoolMetadataServiceProvider;
-import com.proquest.mtg.dismetadataservice.rest.SubjectsMetadataServiceProvider;
+//import com.proquest.mtg.dismetadataservice.rest.SchoolMetadataServiceProvider;
+//import com.proquest.mtg.dismetadataservice.rest.SubjectsMetadataServiceProvider;
 //import com.proquest.mtg.dismetadataservice.rest.FOPEligiblePubsServiceProvider;
 
 public class MockServiceTestHelper extends ServiceTestHelperBase {
 	
 	public MockServiceTestHelper(final MetaDataFormatFactory metaDataFormatFactory,
-			final SchoolMetadataServiceProvider schoolMetadata, 
-			final SubjectsMetadataServiceProvider subjectsMetadata, 
+			//final SchoolMetadataServiceProvider schoolMetadata, 
+			//final SubjectsMetadataServiceProvider subjectsMetadata, 
 			final LocMetaDataServiceProvider locMetadata,
 			final LocReportServiceProvider locReport,
 			final PdfDownloadServiceProvider pdfDownload,
 			//final MStarXmlMetadataServiceProvider mstarMetadata,
-			final ExternalUrlXmlServiceProvider externalUrlData,
+			//final ExternalUrlXmlServiceProvider externalUrlData,
 			//final FOPEligiblePubsServiceProvider fopEligiblePubsData,
 			final FOPUpdateAvailableFormatsServiceProvider fopUpdateAvailableFormats)
 			throws IllegalArgumentException, IOException {
@@ -34,13 +34,13 @@ public class MockServiceTestHelper extends ServiceTestHelperBase {
 									 @Override
 									 protected void configure() {
 										 bind(MetaDataFormatFactory.class).toInstance(metaDataFormatFactory);
-										 bind(SchoolMetadataServiceProvider.class).toInstance(schoolMetadata);
-										 bind(SubjectsMetadataServiceProvider.class).toInstance(subjectsMetadata);
+										 //bind(SchoolMetadataServiceProvider.class).toInstance(schoolMetadata);
+										 //bind(SubjectsMetadataServiceProvider.class).toInstance(subjectsMetadata);
 										 bind(LocMetaDataServiceProvider.class).toInstance(locMetadata);
 										 bind(LocReportServiceProvider.class).toInstance(locReport);
 										 bind(PdfDownloadServiceProvider.class).toInstance(pdfDownload);
 										 //bind(MStarXmlMetadataServiceProvider.class).toInstance(mstarMetadata);
-										 bind(ExternalUrlXmlServiceProvider.class).toInstance(externalUrlData);
+										 //bind(ExternalUrlXmlServiceProvider.class).toInstance(externalUrlData);
 										 //bind(FOPEligiblePubsServiceProvider.class).toInstance(fopEligiblePubsData);
 										 bind(FOPUpdateAvailableFormatsServiceProvider.class).toInstance(fopUpdateAvailableFormats);
 									 }
