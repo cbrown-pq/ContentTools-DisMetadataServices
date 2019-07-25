c0b6b999871305e8ce9e4c247d2e643cd2cc4149
cbrown-pq <charles.brown@proquest.com>
2019-07-24 20:36:40 -0400

    Update IFopMetaDataProvider.java
    import subjects not used

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/fop/IFopMetaDataProvider.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/fop/IFopMetaDataProvider.java
@@ -2,7 +2,7 @@ package com.proquest.mtg.dismetadataservice.fop;
 
 import java.util.List;
 
-import com.proquest.mtg.dismetadataservice.subjects.xml.Subjects.Subject;
+//import com.proquest.mtg.dismetadataservice.subjects.xml.Subjects.Subject;
 
 public interface IFopMetaDataProvider {
 
