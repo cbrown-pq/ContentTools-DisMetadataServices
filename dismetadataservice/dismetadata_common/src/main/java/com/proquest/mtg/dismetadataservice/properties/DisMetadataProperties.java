e6fce339f00174afa87f2f284a9c4e2696f52682
cbrown-pq <charles.brown@proquest.com>
2019-07-25 19:59:22 -0400

    Update DisMetadataProperties.java
    Removed jdbc and Exodus variables.

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/properties/DisMetadataProperties.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/properties/DisMetadataProperties.java
@@ -5,22 +5,22 @@ import java.util.Properties;
 
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
-import com.proquest.mtg.dismetadataservice.jdbc.JdbcConfig;
+//import com.proquest.mtg.dismetadataservice.jdbc.JdbcConfig;
 
 public class DisMetadataProperties {
 
-	public final static String EXODUS_DB_URL = "exodus.db.url";
-	public final static String EXODUS_USER_NAME = "exodus.db.username";
-	public final static String EXODUS_PASSWORD = "exodus.db.password";
-	public final static String EXODUS_POOL_SIZE = "exodus.db.poolsize";
-	public final static String EXODUS_DB_CLASSTYPE = "exodus.db.classtype";
+	//public final static String EXODUS_DB_URL = "exodus.db.url";
+	//public final static String EXODUS_USER_NAME = "exodus.db.username";
+	//public final static String EXODUS_PASSWORD = "exodus.db.password";
+	//public final static String EXODUS_POOL_SIZE = "exodus.db.poolsize";
+	//public final static String EXODUS_DB_CLASSTYPE = "exodus.db.classtype";
 	public final static String PQ_OPEN_URL_BASE = "pq.open.url.base";
 	public static final String SCHOOL_BATCH_SIZE = "school.batch.size";
 	public final static String PQ_SERVICE_URL_BASE = "pq.service.url.base";
 	public final static String PQ_SERVICE_TIMEOUT_MS = "pq.service.timeout.ms";
 	public final static String PQ_SERVICE_USER_AGENT = "pq.service.user.agent";
-	public final static String FOP_EXODUS_USER_NAME = "fop.exodus.db.username";
-	public final static String FOP_EXODUS_PASSWORD = "fop.exodus.db.password";
+	//public final static String FOP_EXODUS_USER_NAME = "fop.exodus.db.username";
+	//public final static String FOP_EXODUS_PASSWORD = "fop.exodus.db.password";
 	public final static String MR3_SERVICE_URL_BASE = "mr3.service.url.base";
 	public final static String ECMS_SERVICE_URL_BASE = "ecms.service.url.base";
 	public final static String ECMS_MR3_HEADER_KEY = "ecms.mr3.header.key";
@@ -36,18 +36,18 @@ public class DisMetadataProperties {
 	
 	public final static ArrayList<String> kRequiredProps = Lists
 			.newArrayList(
-					EXODUS_DB_URL,
-					EXODUS_USER_NAME,
-					EXODUS_PASSWORD,
-					EXODUS_POOL_SIZE,
-					EXODUS_DB_CLASSTYPE,
+					//EXODUS_DB_URL,
+					//EXODUS_USER_NAME,
+					//EXODUS_PASSWORD,
+					//EXODUS_POOL_SIZE,
+					//EXODUS_DB_CLASSTYPE,
 					PQ_OPEN_URL_BASE,
 					SCHOOL_BATCH_SIZE,
 					PQ_SERVICE_URL_BASE,
 					PQ_SERVICE_TIMEOUT_MS,
 					PQ_SERVICE_USER_AGENT,
-					FOP_EXODUS_USER_NAME,
-					FOP_EXODUS_PASSWORD,
+					//FOP_EXODUS_USER_NAME,
+					//FOP_EXODUS_PASSWORD,
 					MR3_SERVICE_URL_BASE,
 					ECMS_SERVICE_URL_BASE,
 					ECMS_MR3_HEADER_KEY,
@@ -62,13 +62,13 @@ public class DisMetadataProperties {
 					FOP_DB_CLASS_NAME);
 	
 	
-	private final JdbcConfig exodusConfig;
+	//private final JdbcConfig exodusConfig;
 	private final String pqOpenUrlBase;
 	private final int schoolBatchSize;
 	private final String pqServiceURL;
 	private final int pqServiceTimeoutMS;
 	private final String pqServiceUserAgent;
-	private final JdbcConfig fopExodusConfig;
+	//private final JdbcConfig fopExodusConfig;
 	private final String mr3ServiceURL;
 	private final String ecmsServiceURL;
 	private final String ecmsMr3HeaderKey;
@@ -90,19 +90,19 @@ public class DisMetadataProperties {
 	public DisMetadataProperties(Properties props) {
 		validate(props);
 		
-		this.exodusConfig = new JdbcConfig(
-				props.getProperty(EXODUS_DB_URL),
-				props.getProperty(EXODUS_USER_NAME),
-				props.getProperty(EXODUS_PASSWORD),
-				props.getProperty(EXODUS_DB_CLASSTYPE),
-				getIntValueFrom(props, EXODUS_POOL_SIZE));
+		//this.exodusConfig = new JdbcConfig(
+		//		props.getProperty(EXODUS_DB_URL),
+		//		props.getProperty(EXODUS_USER_NAME),
+		//		props.getProperty(EXODUS_PASSWORD),
+		//		props.getProperty(EXODUS_DB_CLASSTYPE),
+		//		getIntValueFrom(props, EXODUS_POOL_SIZE));
 		
-		this.fopExodusConfig = new JdbcConfig(
-				props.getProperty(EXODUS_DB_URL),
-				props.getProperty(FOP_EXODUS_USER_NAME),
-				props.getProperty(FOP_EXODUS_PASSWORD),
-				props.getProperty(EXODUS_DB_CLASSTYPE),
-				getIntValueFrom(props, EXODUS_POOL_SIZE));
+		//this.fopExodusConfig = new JdbcConfig(
+		//		props.getProperty(EXODUS_DB_URL),
+		//		props.getProperty(FOP_EXODUS_USER_NAME),
+		//		props.getProperty(FOP_EXODUS_PASSWORD),
+		//		props.getProperty(EXODUS_DB_CLASSTYPE),
+		//		getIntValueFrom(props, EXODUS_POOL_SIZE));
 		
 		this.pqOpenUrlBase = props.getProperty(PQ_OPEN_URL_BASE);
 		this.schoolBatchSize = getIntValueFrom(props, SCHOOL_BATCH_SIZE);
@@ -123,13 +123,13 @@ public class DisMetadataProperties {
 		this.fopDbClass = props.getProperty(FOP_DB_CLASS_NAME);
 	}
 
-	public JdbcConfig getExodusJdbcConfig() {
-		return exodusConfig;
-	}
+	//public JdbcConfig getExodusJdbcConfig() {
+	//	return exodusConfig;
+	//}
 	
-	public JdbcConfig getFopExodusConfig() {
-		return fopExodusConfig;
-	}
+	//public JdbcConfig getFopExodusConfig() {
+	//	return fopExodusConfig;
+	//}
 
 	public String getPqOpenUrlBase() {
 		return pqOpenUrlBase;
