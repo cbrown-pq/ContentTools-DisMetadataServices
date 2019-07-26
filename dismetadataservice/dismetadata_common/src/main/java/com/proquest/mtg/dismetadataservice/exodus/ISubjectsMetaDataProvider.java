5dff5202497200ada4d055b69fa8d73f508c76cc
cbrown-pq <charles.brown@proquest.com>
2019-07-25 20:10:59 -0400

    Removed, unused.
    Exodus retirement.

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/exodus/ISubjectsMetaDataProvider.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/exodus/ISubjectsMetaDataProvider.java
@@ -1,10 +1,10 @@
-package com.proquest.mtg.dismetadataservice.exodus;
-
-import java.util.List;
-
-import com.proquest.mtg.dismetadataservice.subjects.xml.Subjects.Subject;
-
-public interface ISubjectsMetaDataProvider {
-
-	List<Subject> getAllSubjects() throws Exception;
-}
+//package com.proquest.mtg.dismetadataservice.exodus;
+//
+//import java.util.List;
+//
+//import com.proquest.mtg.dismetadataservice.subjects.xml.Subjects.Subject;
+//
+//public interface ISubjectsMetaDataProvider {
+//
+//	List<Subject> getAllSubjects() throws Exception;
+//}
