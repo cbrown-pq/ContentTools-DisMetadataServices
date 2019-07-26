a97d0de9209ae33cbc5721dcabfed4696957c496
cbrown-pq <charles.brown@proquest.com>
2019-07-25 20:15:56 -0400

    Removed, unused.
    Exodus retirement.

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/format/ExternalUrlMetaDataFormatFactory.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/format/ExternalUrlMetaDataFormatFactory.java
@@ -1,46 +1,46 @@
-package com.proquest.mtg.dismetadataservice.format;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.xml.sax.SAXException;
-
-import com.google.inject.Inject;
-import com.proquest.mtg.dismetadataservice.exodus.IExternalUrlDataProvider;
-import com.proquest.mtg.dismetadataservice.externalurl.xml.DissertationList;
-import com.proquest.mtg.dismetadataservice.utils.XmlSchemaValidator;
-
-public class ExternalUrlMetaDataFormatFactory {
-	public static String kDissertationXSDFileName = "ExodusExternalUrl.xsd";
-	private XmlSchemaValidator<DissertationList> xmlSchemaValidator;
-	private final IExternalUrlDataProvider externalUrlDataProvider;
-
-	@Inject
-	public ExternalUrlMetaDataFormatFactory (
-			IExternalUrlDataProvider externalUrlDataProvider) throws IOException, SAXException {
-		this.externalUrlDataProvider = externalUrlDataProvider;
-		ClassLoader classLoader = Thread.currentThread()
-				.getContextClassLoader();
-		InputStream in = classLoader
-				.getResourceAsStream(kDissertationXSDFileName);
-		if (null == in) {
-			throw new IOException("Failed to getSystemResourceAsStream = "
-					+ kDissertationXSDFileName);
-		}
-		xmlSchemaValidator = new XmlSchemaValidator<DissertationList>(in);
-	}
-
-	public IExternalUrlDataProvider getExternalUrlDataProvider() {
-		return externalUrlDataProvider;
-	}
-
-	public ExternalUrlDataFormat get() throws IOException, SAXException {
-		return new ExternalUrlDataFormat(getExternalUrlDataProvider(),
-				xmlSchemaValidator);
-	}
-	
-	public IExternalUrlDataFormat update() throws IOException, SAXException {
-			return new ExternalUrlDataFormat(getExternalUrlDataProvider());
-	}
-
-}
+//package com.proquest.mtg.dismetadataservice.format;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+//import org.xml.sax.SAXException;
+//
+//import com.google.inject.Inject;
+//import com.proquest.mtg.dismetadataservice.exodus.IExternalUrlDataProvider;
+//import com.proquest.mtg.dismetadataservice.externalurl.xml.DissertationList;
+//import com.proquest.mtg.dismetadataservice.utils.XmlSchemaValidator;
+//
+//public class ExternalUrlMetaDataFormatFactory {
+//	public static String kDissertationXSDFileName = "ExodusExternalUrl.xsd";
+//	private XmlSchemaValidator<DissertationList> xmlSchemaValidator;
+//	private final IExternalUrlDataProvider externalUrlDataProvider;
+//
+//	@Inject
+//	public ExternalUrlMetaDataFormatFactory (
+//			IExternalUrlDataProvider externalUrlDataProvider) throws IOException, SAXException {
+//		this.externalUrlDataProvider = externalUrlDataProvider;
+//		ClassLoader classLoader = Thread.currentThread()
+//				.getContextClassLoader();
+//		InputStream in = classLoader
+//				.getResourceAsStream(kDissertationXSDFileName);
+//		if (null == in) {
+//			throw new IOException("Failed to getSystemResourceAsStream = "
+//					+ kDissertationXSDFileName);
+//		}
+//		xmlSchemaValidator = new XmlSchemaValidator<DissertationList>(in);
+//	}
+//
+//	public IExternalUrlDataProvider getExternalUrlDataProvider() {
+//		return externalUrlDataProvider;
+//	}
+//
+//	public ExternalUrlDataFormat get() throws IOException, SAXException {
+//		return new ExternalUrlDataFormat(getExternalUrlDataProvider(),
+//				xmlSchemaValidator);
+//	}
+//	
+//	public IExternalUrlDataFormat update() throws IOException, SAXException {
+//			return new ExternalUrlDataFormat(getExternalUrlDataProvider());
+//	}
+//
+//}
