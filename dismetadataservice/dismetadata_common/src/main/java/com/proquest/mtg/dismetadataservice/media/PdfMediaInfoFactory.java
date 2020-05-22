17eface111b1943525975798568974d837b41191
mnarayanpq <Mahitha.Narayan@proquest.com>
2020-05-22 10:42:14 -0400

    removed log statements

--- dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/media/PdfMediaInfoFactory.java
+++ dismetadataservice/dismetadata_common/src/main/java/com/proquest/mtg/dismetadataservice/media/PdfMediaInfoFactory.java
@@ -64,18 +64,12 @@ public class PdfMediaInfoFactory {
 	}
 
 	private boolean isFullText(Comp curComponent) {
-	   System.out.println(null != curComponent &&
-			curComponent.getType().equalsIgnoreCase(kFullText));
 		return 
 			null != curComponent &&
 			curComponent.getType().equalsIgnoreCase(kFullText) ;
 	}
 
 	private boolean isFullTextPdf(Rep curRepresentation) {
-		System.out.println(null != curRepresentation &&
-				curRepresentation.getType().equalsIgnoreCase(kPdfFullText) &&
-				null != curRepresentation.getPdfType() &&
-				null != curRepresentation.getPath());
 		return
 			null != curRepresentation &&
 			curRepresentation.getType().equalsIgnoreCase(kPdfFullText) &&
