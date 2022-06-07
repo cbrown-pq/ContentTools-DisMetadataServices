ff9e100c6edd524fcd23c5e0059ae648051486cb
cbrown-pq <charles.brown@proquest.com>
2022-06-07 13:02:46 -0400

    Update Marc21RdaRecordFactory_AccessNoteRestriction_Tests.java

--- dismetadataservice/dismetadata_common/src/test/java/com/proquest/mtg/dismetadataservice/marc21rda/Marc21RdaRecordFactory_AccessNoteRestriction_Tests.java
+++ dismetadataservice/dismetadata_common/src/test/java/com/proquest/mtg/dismetadataservice/marc21rda/Marc21RdaRecordFactory_AccessNoteRestriction_Tests.java
@@ -43,33 +43,33 @@ public class Marc21RdaRecordFactory_AccessNoteRestriction_Tests extends
 		assertThat(fieldsMatchingTag.size(), is(0));
 	}
 
-	@Test
-	public void withSalesRestriction5_DoNotSell_ThirdPartyVendor()
-			throws Exception {
-		String salesRestrictioNote = "This item must not be sold to any third party vendors";
-		DisPubMetaData metaData = new DisPubMetaData();
-		salesRestriction = new SalesRestriction();
-		metaData.setPubNumber(Marc21RdaRecordFactory_Test_Helper.kPubForPdfAvailable);
-		salesRestriction.setCode("5");
-		metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction));
-		expectedMarcFieldData = "  " + MarcCharSet.kSubFieldIndicator + "a"
-				+ salesRestrictioNote + ".";
-		verifyMarcRecordHasCorrectField(metaData, tag, expectedMarcFieldData, 1);
-	}
+	//@Test
+	//public void withSalesRestriction5_DoNotSell_ThirdPartyVendor()
+	//		throws Exception {
+	//	String salesRestrictioNote = "This item must not be sold to any third party vendors";
+	//	DisPubMetaData metaData = new DisPubMetaData();
+	//	salesRestriction = new SalesRestriction();
+	//	metaData.setPubNumber(Marc21RdaRecordFactory_Test_Helper.kPubForPdfAvailable);
+	//	salesRestriction.setCode("5");
+	//	metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction));
+	//	expectedMarcFieldData = "  " + MarcCharSet.kSubFieldIndicator + "a"
+	//			+ salesRestrictioNote + ".";
+	//	verifyMarcRecordHasCorrectField(metaData, tag, expectedMarcFieldData, 1);
+	//}
 
-	@Test
-	public void withSalesRestriction5_DoNotSell_ThirdPartySearchIndex()
-			throws Exception {
-		String salesRestrictioNote = "This item must not be added to any third party search indexes";
-		DisPubMetaData metaData = new DisPubMetaData();
-		metaData.setPubNumber(Marc21RdaRecordFactory_Test_Helper.kPubForPdfAvailable);
-		salesRestriction = new SalesRestriction();
-		salesRestriction.setCode("8");
-		metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction));
-		expectedMarcFieldData = "  " + MarcCharSet.kSubFieldIndicator + "a"
-				+ salesRestrictioNote + ".";
-		verifyMarcRecordHasCorrectField(metaData, tag, expectedMarcFieldData, 1);
-	}
+	//@Test
+	//public void withSalesRestriction5_DoNotSell_ThirdPartySearchIndex()
+	//		throws Exception {
+	//	String salesRestrictioNote = "This item must not be added to any third party search indexes";
+	//	DisPubMetaData metaData = new DisPubMetaData();
+	//	metaData.setPubNumber(Marc21RdaRecordFactory_Test_Helper.kPubForPdfAvailable);
+	//	salesRestriction = new SalesRestriction();
+	//	salesRestriction.setCode("8");
+	//	metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction));
+	//	expectedMarcFieldData = "  " + MarcCharSet.kSubFieldIndicator + "a"
+	//			+ salesRestrictioNote + ".";
+	//	verifyMarcRecordHasCorrectField(metaData, tag, expectedMarcFieldData, 1);
+	//}
 
 	@Test
 	public void withSalesRestriction_AllOtherRestrictions() throws Exception {
@@ -84,40 +84,40 @@ public class Marc21RdaRecordFactory_AccessNoteRestriction_Tests extends
 		verifyMarcRecordHasCorrectField(metaData, tag, expectedMarcFieldData, 1);
 	}
 
-	@Test
-	public void withMultipleSalesRestrictions() throws Exception {
-		String pubId = "test pub";
-		String expectedMarcFieldData1 = "  "
-				+ MarcCharSet.kSubFieldIndicator
-				+ "a"
-				+ "This item is not available from ProQuest Dissertations & Theses.";
-		String expectedMarcFieldData2 = "  "
-				+ MarcCharSet.kSubFieldIndicator
-				+ "a"
-				+ "This item must not be added to any third party search indexes.";
-		String expectedMarcFieldData3 = "  " + MarcCharSet.kSubFieldIndicator
-				+ "a"
-				+ "This item must not be sold to any third party vendors.";
-
-		DisPubMetaData metaData = new DisPubMetaData();
-		metaData.setPubNumber(pubId);
-		SalesRestriction salesRestriction1 = new SalesRestriction();
-		salesRestriction1.setCode("1");
-		SalesRestriction salesRestriction2 = new SalesRestriction();
-		salesRestriction2.setCode("8");
-		SalesRestriction salesRestriction3 = new SalesRestriction();
-		salesRestriction3.setCode("5");
-		metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction1,
-				salesRestriction2, salesRestriction3));
-		MarcRecord marc = factory.makeFrom(metaData);
-		List<MarcField> salesRestrictionFields = marc.getFieldsMatchingTag(tag);
-		assertThat(salesRestrictionFields.size(), is(3));
-		assertThat(salesRestrictionFields.get(0).getData(),
-				is(expectedMarcFieldData1));
-		assertThat(salesRestrictionFields.get(1).getData(),
-				is(expectedMarcFieldData2));
-		assertThat(salesRestrictionFields.get(2).getData(),
-				is(expectedMarcFieldData3));
-	}
+	//@Test
+	//public void withMultipleSalesRestrictions() throws Exception {
+	//	String pubId = "test pub";
+	//	String expectedMarcFieldData1 = "  "
+	//			+ MarcCharSet.kSubFieldIndicator
+	//			+ "a"
+	//			+ "This item is not available from ProQuest Dissertations & Theses.";
+	//	String expectedMarcFieldData2 = "  "
+	//			+ MarcCharSet.kSubFieldIndicator
+	//			+ "a"
+	//			+ "This item must not be added to any third party search indexes.";
+	//	String expectedMarcFieldData3 = "  " + MarcCharSet.kSubFieldIndicator
+	//			+ "a"
+	//			+ "This item must not be sold to any third party vendors.";
+//
+	//	DisPubMetaData metaData = new DisPubMetaData();
+	//	metaData.setPubNumber(pubId);
+	//	SalesRestriction salesRestriction1 = new SalesRestriction();
+	//	salesRestriction1.setCode("1");
+	//	SalesRestriction salesRestriction2 = new SalesRestriction();
+	//	salesRestriction2.setCode("8");
+	//	SalesRestriction salesRestriction3 = new SalesRestriction();
+	//	salesRestriction3.setCode("5");
+	//	metaData.setSalesRestrictions(Lists.newArrayList(salesRestriction1,
+	//			salesRestriction2, salesRestriction3));
+	//	MarcRecord marc = factory.makeFrom(metaData);
+	//	List<MarcField> salesRestrictionFields = marc.getFieldsMatchingTag(tag);
+	//	assertThat(salesRestrictionFields.size(), is(3));
+	//	assertThat(salesRestrictionFields.get(0).getData(),
+	//			is(expectedMarcFieldData1));
+	//	assertThat(salesRestrictionFields.get(1).getData(),
+	//			is(expectedMarcFieldData2));
+	//	assertThat(salesRestrictionFields.get(2).getData(),
+	//			is(expectedMarcFieldData3));
+	//}
 
 }
